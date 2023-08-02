c82ed8591f932aecaa890d63842f6dc8e3240c60
Yoonzero <132437163+Yooonzero@users.noreply.github.com>
2023-08-02 14:27:34 +0900

    [level 0] Title: 편지, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120898. 편지/편지.js
@@ -0,0 +1,8 @@
+function solution(message) {
+    var answer = 0;
+    a = message.length;
+    b = message.length * 2;
+    // answer = `message의 글자 수가 ${a}개로 최소 가로 ${b}cm의 편지지가 필요합니다.`;
+    answer = b
+    return answer;
+}
